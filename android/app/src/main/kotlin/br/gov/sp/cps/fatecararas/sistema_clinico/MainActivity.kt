29df9e6f8948db779534be3158df05f628c13014
Frank <33494411+frqnk@users.noreply.github.com>
2025-06-09 22:01:22 -0300

    refactor: android project reset

---
+++ android/app/src/main/kotlin/br/gov/sp/cps/fatecararas/sistema_clinico/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.gov.sp.cps.fatecararas.sistema_clinico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/sistema_clinico/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sistema_clinico
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
